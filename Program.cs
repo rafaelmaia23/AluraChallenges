b04eb3b6783add44b475479081b68c88cd0a07a9
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-08 16:56:21 -0300

    added sql server connection string

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using AluraChallenges.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.servi
+builder.Services.AddDbContext<AppDbContext>(opt => opt
+    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
