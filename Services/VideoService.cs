35fa58f044658726d145fbe0269f3548491da196
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-23 15:11:03 -0300

    Added GetVideoAsync EndPoint and Service with FluentResults

---
+++ Services/VideoService.cs
@@ -0,0 +1,21 @@
+﻿using AluraChallenges.Data;
+using AluraChallenges.Models;
+using AluraChallenges.Services.IService;
+using AutoMapper;
+using FluentResults;
+
+namespace AluraChallenges.Services;
+
+public class VideoService : IVideoService
+{
+    private readonly AppDbContext _db;
+    private readonly IMapper mapper
+    public VideoService()
+    {
+
+    }
+    public Task<Result<List<Video>>> GetVideosAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
