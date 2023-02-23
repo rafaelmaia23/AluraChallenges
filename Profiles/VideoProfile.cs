57b4335896c0c06283c4f7484291f2abd3317574
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-23 15:49:50 -0300

    Added automapper service to promgram.cs and profile folder with video Profile maps

---
+++ Profiles/VideoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AluraChallenges.Models;
+using AluraChallenges.Models.VideoDto;
+using AutoMapper;
+
+namespace AluraChallenges.Profiles;
+
+public class VideoProfile : Profile
+{
+	public VideoProfile()
+	{
+		CreateMap<CreateVideoDto, Video>();
+		CreateMap<Video, ReadVideoDto>();
+	}
+}
