cce48c0ce1f90b306cf40407ff1dd2ad2c40be51
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-24 21:17:51 -0300

    Added UpdateVideoDto Model

---
+++ Models/VideoDto/UpdateVideoDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges.Models.VideoDto;
+
+public class UpdateVideoDto
+{
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Url { get; set; }
+}
