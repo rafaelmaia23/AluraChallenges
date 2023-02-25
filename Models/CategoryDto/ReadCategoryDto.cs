92865dd4dfa34cf57124a034513eb86048da0679
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-25 14:13:08 -0300

    Added Category Model with Create, Read and Update Dtos

---
+++ Models/CategoryDto/ReadCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges.Models.CategoryDto;
+
+public class ReadCategoryDto
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Color { get; set; }
+}
---
+++ Models/CategoryDto/UpdateCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges.Models.CategoryDto;
+
+public class UpdateCategoryDto
+{
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Color { get; set; }
+}
