75266b4ba4a865716b209661c71969d43eb3e1bc
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-25 14:23:34 -0300

    Added category service to dependency injection in program class

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddScoped<IVideoService, VideoService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
