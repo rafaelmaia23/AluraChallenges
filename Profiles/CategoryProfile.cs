1e18d0288536f5e47f8a9027b0f26d64d5459b3e
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-02-25 16:04:07 -0300

    Added Category Profile class with AutoMapper

--- Profiles/CategoryProfile.cs
+++ Profiles/CategoryProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace AluraChallenges.Profiles
+﻿using AluraChallenges.Models;
+using AluraChallenges.Models.CategoryDto;
+using AutoMapper;
+
+namespace AluraChallenges.Profiles;
+
+public class CategoryProfile : Profile
 {
-    public class CategoryProfile
-    {
-    }
+	public CategoryProfile()
+	{
+		CreateMap<CreateCategoryDto, Category>();
+		CreateMap<Category, ReadCategoryDto>();
+		CreateMap<UpdateCategoryDto, Category>();
+	}
 }
