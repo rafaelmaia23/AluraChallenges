9dd21b3a9fd957b2644ddc6d15af2c363f8e3d7c
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-01 16:30:15 -0300

    User now inherit from IdentityUser

--- Models/User.cs
+++ Models/User.cs
@@ -1,21 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace AluraChallenges.Models;
 
-public class User
+public class User : IdentityUser
 {
-    [Required]
-    [Key]
-    public int Id { get; set; }
-    [Required]
-    public string UserName { get; set; }
-    [Required]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
-    [Required]
-    public string Email { get; set; }
-    [Required]
-    public string Role { get; set; }
     [Required]
     public string Name { get; set; }
     [Required]
