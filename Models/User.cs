b65e325c678b24aafeeff5a9570bea14b154bf9d
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-01 15:53:32 -0300

    Added User, CreateUserDto UserLogin Models

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges.Models;
+
+public class User
+{
+    [Required]
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Role { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Surname { get; set; }
+}
---
+++ Models/UserDto/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraChallenges.Models.UserDto;
+
+public class UserLogin
+{
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
