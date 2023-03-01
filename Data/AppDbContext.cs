6bbcd3594e5f13e15b5da9d257cc2456b6ec9de7
Rafael Maia <86494551+rafaelmaia23@users.noreply.github.com>
2023-03-01 16:32:43 -0300

    AppDbCOntext now inherit from IdentityDbContext<User>

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using AluraChallenges.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AluraChallenges.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<User>
 {
 	public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
 	{
